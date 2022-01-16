267735d0013993993c7bfe655f4e32e9c51f0c0f
oliverlkelly <18181648+oliverlkelly@users.noreply.github.com>
2022-01-16 19:13:50 +1100

    creating initial api call from city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-var searchBox = document.getElementsByClassName("searchBox");
-var searchBtn = document.getElementsByClassName("searchBtn");
+var searchBox = document.querySelector("#searchBox");
+var searchBtn = document.querySelector("#searchBtn");
 var city;
+var cityHistory = [];
 var appID = "6bf490b8fe0c71916ca8e76e4a98d42c";
-var apiURL = `api.openweathermap.org/data/2.5/weather?q="${city}"&appid="${appID}"`;
+var apiURLCity = `api.openweathermap.org/data/2.5/weather?q="${city}"&appid="${appID}"`;
 
-function(){
+function setCity(){
     city = searchBox.value;
-    console.log(city);
+    apiURL = `api.openweathermap.org/data/2.5/weather?q="${city}"&appid="${appID}"`;
 }
+
+searchBtn.addEventListener("click", function(){
+    setCity();
+});
