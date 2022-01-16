5156c337d57bbc9e6aa3aa96ef446dac1767eba2
oliverlkelly <18181648+oliverlkelly@users.noreply.github.com>
2022-01-16 21:06:18 +1100

    got api calls and errors working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,9 @@ var city;
 var cityHistory = [];
 var appID = "6bf490b8fe0c71916ca8e76e4a98d42c";
 var apiURLCity = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+appID;
-var apiURLOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=`+appID;
+var apiURLOneCall;
 var cityAPICall;
+var oneCallAPICall;
 
 function setCity(){
     city = searchBox.value;
@@ -30,17 +31,35 @@ function callCityAPI(){
 
         })
         .catch(function(error){
-            window.confirm("Please enter a valid city.");
+            window.confirm(`${error}\nPlease enter a valid city.`);
         })
 }
 function callOneCall(lon, lat){
-
+    apiURLOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=`+appID;
+    fetch(apiURLOneCall, {
+        method: 'GET',
+        redirect: 'follow',
+    })
+        .then(function(response){
+            if(response.status >= 200 && response.status <= 299){
+                return response.json();
+            }
+            else{
+                throw Error(response.statusText);
+            }
+        })
+        .then(function(data){
+            oneCallAPICall = data;
+        })
+        .catch(function(error){
+            console.log(error);
+        })
 }
 
 searchBtn.addEventListener("click", function(){
     setCity();
-    cityAPICall = callCityAPI();
+    callCityAPI();
     if(cityAPICall !== undefined){
-
+        callOneCall(cityAPICall.coord.lon, cityAPICall.coord.lat);
     }
 });
